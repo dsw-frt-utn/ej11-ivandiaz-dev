9f348cad9433443bbfac4b243324a1ceb4938ce0
Ivan Diaz <ivandiaz.arg.dev@gmail.com>
2026-05-22 19:29:30 -0300

    Resolucion del problema

--- Dsw2026Ej11/Collections/CasoList.cs
+++ Dsw2026Ej11/Collections/CasoList.cs
@@ -1,4 +1,6 @@
-﻿namespace Dsw2026Ej11.Collections;
+﻿using Dsw2026Ej11.Domain;
+
+namespace Dsw2026Ej11.Collections;
 
 //Crear un campo que represente una lista de alumnos (List<>)
 //Incluir un método para agregar alumnos a la lista
@@ -6,6 +8,43 @@
 //Incluir un método para buscar un alumno por nombre
 //Incluir un método para eliminar un alumno (debe recibir un alumno)
 //Incluir un método para eliminar un alumno en una determinada posición de la lista
-public class CasoList
+public class CasoList<T> where T : Alumno
 {
+    private List<T> _alumnos = new List<T>();
+
+    public void AgregarAlumno(T alumno)
+    {
+        _alumnos.Add(alumno);
+    }
+
+    public List<T> GetAlumnos()
+    {
+        return _alumnos;
+    }
+
+    public string BuscarAlumno(string nombre)
+    {
+        foreach (Alumno a in _alumnos)
+        {
+            if (a.Nombre == nombre)
+            {
+                return a.ToString();
+            }
+        }
+        return "No existe";
+    }
+
+    public void EliminarAlumno(T alumno)
+    {
+        _alumnos.Remove(alumno);
+    }
+
+    public void ELiminarIndice(int i)
+    {
+        if (i < _alumnos.Count && i >= 0)
+        {
+            _alumnos.RemoveAt(i);
+        }
+    }
 }
+
